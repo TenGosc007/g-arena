0f44d374edd0a6f7c3dd9d057a9251ad150f2343
Aleksander Atamanczuk <aleksander.atamanczuk@gmail.com>
2026-05-10 19:12:24 +0200

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importX from "eslint-plugin-import-x";
+import perfectionist from "eslint-plugin-perfectionist";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +16,32 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  reactHooks.configs.flat.recommended,
+  {
+    plugins: { "import-x": importX, perfectionist },
+    rules: {
+      "import-x/no-cycle": "error",
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          type: "alphabetical",
+          order: "asc",
+          ignoreCase: true,
+          newlinesBetween: 1,
+          tsconfig: { rootDir: "." },
+          internalPattern: ["^@/"],
+          groups: [
+            "type-import",
+            ["value-builtin", "value-external"],
+            "value-internal",
+            ["value-parent", "value-sibling", "value-index"],
+            "side-effect",
+            "unknown",
+          ],
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
